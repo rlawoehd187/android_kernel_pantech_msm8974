cce2e6565a5f3f7b84b58106851df05c84afc7b4
Vladis Dronov <vdronov@redhat.com>
2015-12-24 11:09:41 -0500

    selinux: rate-limit netlink message warnings in selinux_nlmsg_perm()
    commit 76319946f321e30872dd72af7de867cb26e7a373 upstream.
    
    Any process is able to send netlink messages with invalid types.
    Make the warning rate-limited to prevent too much log spam.
    
    The warning is supposed to help to find misbehaving programs, so
    print the triggering command name and pid.
    
    Reported-by: Florian Weimer <fweimer@redhat.com>
    Signed-off-by: Vladis Dronov <vdronov@redhat.com>
    [PM: subject line tweak to make checkpatch.pl happy]
    Signed-off-by: Paul Moore <pmoore@redhat.com>
    Signed-off-by: Ben Hutchings <ben@decadent.org.uk>
    Signed-off-by: Kevin F. Haggerty <haggertk@lineageos.org>
    Change-Id: I518bf279dcf40be64f6d8e26e54671c44e03c9f2

--- security/selinux/hooks.c
+++ security/selinux/hooks.c
@@ -4737,10 +4737,12 @@ static int selinux_nlmsg_perm(struct sock *sk, struct sk_buff *skb)
 	if (err) {
 		if (err == -EINVAL) {
 			printk(KERN_WARNING
-			       "SELinux: unrecognized netlink message:"
-			       " protocol=%hu nlmsg_type=%hu sclass=%s\n",
+			pr_warn_ratelimited("SELinux: unrecognized netlink"
+			       " message: protocol=%hu nlmsg_type=%hu sclass=%s"
+			       " pig=%d comm=%s\n",
 			       sk->sk_protocol, nlh->nlmsg_type,
-			       secclass_map[sksec->sclass - 1].name);
+			       secclass_map[sksec->sclass - 1].name,
+			       task_pid_nr(current), current->comm);
 			if (!selinux_enforcing || security_get_allow_unknown())
 				err = 0;
 		}
