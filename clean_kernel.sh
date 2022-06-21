b797d3c90716435aca04296c674a74f1b9ca68e9
hiru <rlawoehd187@naver.com>
2022-06-21 20:35:40 +0900

    Revert "Fix build"
    This reverts commit 66dfb07494105cb811980139294cd789d220dca0.
    
    Signed-off-by: hiru <rlawoehd187@naver.com>

--- clean_kernel.sh
+++ clean_kernel.sh
@@ -1,17 +1,30 @@
 #!/bin/bash
-###############################################################################
-#
-#                           Kernel Build Script 
-#
-###############################################################################
 
-export ARCH=arm
-export PATH=/home/chautruongthinh/cm13/prebuilts/gcc/linux-x86/arm/arm-linux-androideabi-4.9/bin:$PATH
-export CROSS_COMPILE=arm-linux-androideabi-
+############################################################################
+#kernel build Script
+#
+###############################################################################
+# 2011-10-24 effectivesky : modified
+# 2010-12-29 allydrop     : created
+###############################################################################
+#export ARCH=arm
+#export PATH=$(pwd)/../../toolchain_arm-eabi-4.6/arm-eabi-4.6/bin:$PATH
+#export CROSS_COMPILE=arm-eabi-
+##export PATH=$(pwd)/../prebuilts/gcc/linux-x86/arm/arm-eabi-4.6/bin:$PATH
+
+export PATH=$(pwd)/../../../../arm-eabi-4.6/bin:$PATH
+export CROSS_COMPILE=arm-eabi-
 
 
 make mrproper
-rm -rf ./kernel_log.txt
-rm -rf ./obj
-rm -rf ./zImage
-rm -rf ./dt.img
+make O=./obj/KERNEL_OBJ/ clean
+if [ -f ./zImage ]
+then
+    rm ./zImage
+fi
+
+if [ -d ./obj/ ]
+then
+    rm -r ./obj/
+fi
+
