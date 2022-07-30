82f7bba2602a6d1545d664ccded5905a2c96877f
hiru <rlawoehd187@naver.com>
2022-07-31 05:22:08 +0900

    Revert "Bluetooth: bnep: bnep_add_connection() should verify that it's dealing with l2cap socket"
    This reverts commit 364d1e2ea3f579f93ea8909fc3e5743e858009e0.

--- net/bluetooth/bnep/core.c
+++ net/bluetooth/bnep/core.c
@@ -50,7 +50,6 @@
 #include <asm/unaligned.h>
 
 #include <net/bluetooth/bluetooth.h>
-#include <net/bluetooth/l2cap.h>
 #include <net/bluetooth/hci_core.h>
 #include <net/bluetooth/l2cap.h>
 
@@ -565,9 +564,6 @@ int bnep_add_connection(struct bnep_connadd_req *req, struct socket *sock)
 	int err;
 
 	BT_DBG("");
-
-	if (!l2cap_is_socket(sock))
-		return -EBADFD;
 
 	baswap((void *) dst, &bt_sk(sock->sk)->dst);
 	baswap((void *) src, &bt_sk(sock->sk)->src);
